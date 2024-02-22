8c2e957180d1231acf8e86461f125891415ce922
Tayyaba <tayyaba.afzal@gmail.com>
2024-02-22 15:54:01 +0000

    questions added to the array for inquirer

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,55 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project title:",
 
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project:",
+
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter installation instruction for your project:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage information for your project.",
+    },
+
+    {
+        type: "input",
+        name: "contributionGuidelines",
+        message: "Please enter contribution guidelines for your project.",
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "Please enter test instructions for your project.",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "Please enter your GitHub username.",
+    },
+    {
+        type: "input",
+        name: "emailAddress",
+        message: "Please enter your email address.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license for your project.",
+        choices: ["MIT", "ISC", "Apache", "GPL", "BSD", "None"],
+    }
 ];
 
 // function to write README file
